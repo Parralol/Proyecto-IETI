d402685723dadb54c2e64f3aecc31188f95e9fa1
Parralol <110953563+Parralol@users.noreply.github.com>
2024-09-07 20:58:32 -0500

    Add files via upload

---
+++ src/main/java/com/proyecto/ieti/pemc/PemcApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.ieti.pemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PemcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PemcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/ieti/pemc/PemcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.ieti.pemc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PemcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
