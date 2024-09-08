4b015159b8bf5cc5aaadc2f8d39330d48a1da3b4
parralol <santinico36@hotmail.com>
2024-09-07 21:01:32 -0500

    first change

--- src/test/java/com/proyecto/ieti/pemc/PemcApplicationTests.java
+++ src/test/java/com/proyecto/ieti/pemc/PemcApplicationTests.java
@@ -2,7 +2,7 @@ package com.proyecto.ieti.pemc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// intento
 @SpringBootTest
 class PemcApplicationTests {
 
