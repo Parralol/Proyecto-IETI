8f0c1ba893c6ce8fb8b538009a423bee4dfe4182
parralol <santinico36@hotmail.com>
2024-10-31 00:08:48 -0500

    adaptable

--- src/main/java/com/proyecto/ieti/pemc/config/CustomCorsConfiguration.java
+++ src/main/java/com/proyecto/ieti/pemc/config/CustomCorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.proyecto.ieti.pemc.config;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -13,7 +14,11 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080"));
+        try {
+            config.setAllowedOrigins(List.of("http://" +Runtime.getRuntime().exec("hostname")+":8080"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         return config;
